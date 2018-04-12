0fc5ab9a08bc80d69ddd5918426dd034c0141f08
lekkanmca <lekkanmca@gmail.com>
2018-04-12 09:25:46 +0530

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ class Home extends Component{
 
     render(){
         return(
-            <div className="row m-t-5">
+            <div className="row mt-5">
                 <div className="col-lg-12">
                     <p className=""> Home page content here</p>
                 </div>
