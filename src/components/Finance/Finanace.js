4de07ce6191bfe0d375aad03ecaee14da7fce43d
lekkanm <lekkanm@gmail.com>
2018-05-04 18:14:49 +0530

    Edit Finance

--- src/components/Finance/Finanace.js
+++ src/components/Finance/Finanace.js
@@ -9,11 +9,13 @@ class Finance extends Component{
         super(props);
         this.state={
             finance_cus:[],
+            finance_cusinfo:[0],
             search_lineid:'',
             success_message:'',
             error_message:''
         };
         this.editFinance=this.editFinance.bind(this);
+        this.updateFinCustomer=this.updateFinCustomer.bind(this);
         var cuslist,searchpath="";
         if(this.props.customers.linetypeselected){
            searchpath="?linetype="+this.props.customers.linetypeselected;
@@ -33,7 +35,18 @@ class Finance extends Component{
     }
 
     editFinance(id){
-        console.log("sdfd");
+        fetch(HOST+'finance/get_fincustomer_info.php?id='+id)
+        .then((response)=>response.json())
+        .then((responsejson)=>
+        {
+            this.setState({finance_cusinfo:responsejson[0]});
+        }
+        );
+        console.log(this.state.finance_cusinfo);
+    }
+
+    updateFinCustomer(){
+
     }
     render(){
         return(
@@ -61,7 +74,7 @@ class Finance extends Component{
                                     {this.props.customers.fin_customers.map((customer,i)=>
                                         <tr>
                                             <td>{i+1}</td>
-                                            <td><a onClick={()=>this.editFinance(customer.cus_id)} href="javascript:;" data-toggle="modal" data-target="#editUser">{customer.cus_name}</a></td>
+                                            <td><a onClick={()=>this.editFinance(customer.cus_id)} href="javascript:;" data-toggle="modal" data-target="#editFinCustomer">{customer.cus_name}</a></td>
                                             <td>{customer.area ? customer.area :"-"}</td>
                                         </tr>
                                     )}
@@ -76,6 +89,66 @@ class Finance extends Component{
                                 </tbody>
                             </table>
                         </div>
+                        {/*Edit Modal Start*/}
+                        <div className="modal fade" id="editFinCustomer">
+                            <div className="modal-dialog">
+                                <div className="modal-content">
+                                    <div className="modal-header">
+                                        <div className="modal-title text-center">
+                                            <h4 className="text-info">Edit Customer Info</h4>                                           
+                                        </div>
+                                        <div className="float-right">
+                                                <button className="close" id="updateformclose" data-dismiss="modal">&times;</button>
+                                        </div>
+                                    </div>
+                                    <div className="modal-body">
+                                        <form className="text-dark finCustomerEditForm" onSubmit={this.updateFinCustomer}>
+                                            <div className="form-group">
+                                                <div className="row">
+                                                    <div className="col-lg-4 col-sm-12">
+                                                        <label>Name</label>
+                                                    </div>
+                                                    <div className="col-lg-8 col-sm-12">
+                                                        <input className="form-control cus_name" id="cus_name" required name="cus_name" type="text" value={this.state.finance_cusinfo.cus_name} onChange = {this.handleChange} />
+                                                        <input className="form-control cus_id" id="cus_id" required name="cus_id" type="hidden" value={this.state.finance_cusinfo.cus_id} />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            
+                                            
+                                            <div className="form-group">
+                                                <div className="row">
+                                                    <div className="col-lg-4 col-sm-12">
+                                                        <label>Line(Area)</label>
+                                                    </div>
+                                                    <div className="col-lg-8 col-sm-12">
+                                                        <select className="form-control line_id" name="line_id"  value={this.state.finance_cusinfo.line_id ? this.state.finance_cusinfo.line_id:''} onChange = {this.handleChange} >
+                                                            <option value="">Select Line</option>
+                                                            {/*
+                                                            this.state.lines.length >0 ? (
+                                                                this.state.lines.map((line,i)=>
+                                                                    <option value={line.line_id}>{line.area}</option>
+                                                                )
+                                                            ) :""
+                                                        */ }
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="form-group">
+                                                <div className="row">
+                                                    <div className="col-lg-12 text-center col-sm-12">
+                                                        <button className="btn btn-primary" name="updatecustomer" type="submit">Update</button>
+                                                        <button className="btn btn-warning ml-2" name="cancel close" data-dismiss="modal">Cancel</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        {/*Edit Modal End*/}
                 </div>
             </div>
         );
